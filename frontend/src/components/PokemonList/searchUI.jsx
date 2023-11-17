aaf8c9c336981cd0c15772e0501156a5facc5205
Adam Wentworth <adamjohnwentworth@gmail.com>
2023-11-16 21:27:30 -0800

    added toggle 'show evolutionary line' and it works!

--- frontend/src/components/PokemonList/searchUI.jsx
+++ frontend/src/components/PokemonList/searchUI.jsx
@@ -1,17 +1,19 @@
 /*searchUI.jsx*/
 
 import React from 'react';
-import './searchUI.css';
+import './searchUI.css'; // Your CSS file should include the styles for the evolutionary line checkbox
 
-function SearchUI({ 
-    searchTerm, 
-    onSearchChange, 
-    isShiny, 
-    toggleShiny, 
-    showCostume, 
-    toggleCostume, 
-    showShadow, 
-    toggleShadow 
+function SearchUI({
+    searchTerm,
+    onSearchChange,
+    isShiny,
+    toggleShiny,
+    showCostume,
+    toggleCostume,
+    showShadow,
+    toggleShadow,
+    showEvolutionaryLine, // state to track if the checkbox is checked
+    toggleEvolutionaryLine // function to handle checkbox changes
 }) {
     return (
         <div className="header-section browse-section">
@@ -20,7 +22,7 @@ function SearchUI({
                 type="text" 
                 placeholder="Search..."
                 value={searchTerm} 
-                onChange={(e) => onSearchChange(e.target.value)}                        
+                onChange={(e) => onSearchChange(e.target.value)}
             />
             <button onClick={toggleShiny} className={`shiny-button ${isShiny ? 'active' : ''}`}>
                 <img src="/images/shiny_icon.png" alt="Toggle Shiny" />
@@ -31,6 +33,18 @@ function SearchUI({
             <button onClick={toggleShadow} className={`shadow-button ${showShadow ? 'active' : ''}`}>
                 <img src="/images/shadow_icon.png" alt="Toggle Shadow" />
             </button>
+            {/* Evolutionary Line Checkbox */}
+            <label className="evo-line-checkbox">
+                <input
+                    id="evolutionaryLineCheckbox"
+                    type="checkbox"
+                    className="evo-checkbox"
+                    checked={showEvolutionaryLine}
+                    onChange={toggleEvolutionaryLine}
+                />
+                <span className="evo-line-custom-checkbox" />
+                SHOW EVOLUTIONARY LINE
+            </label>
         </div>
     );
 }
