deba6f1c7835da807a99705e02aae1621b0fd3c8
Adam Wentworth <adamjohnwentworth@gmail.com>
2023-11-18 15:08:21 -0800

    drastically improved css

--- frontend/src/components/PokemonList/searchUI.jsx
+++ frontend/src/components/PokemonList/searchUI.jsx
@@ -18,22 +18,23 @@ function SearchUI({
     return (
         <div className="header-section browse-section">
             <h1>Browse</h1>
+            <div className="button-container"> {/* New container for buttons */}
+                <button onClick={toggleShiny} className={`shiny-button ${isShiny ? 'active' : ''}`}>
+                    <img src="/images/shiny_icon.png" alt="Toggle Shiny" />
+                </button>
+                <button onClick={toggleCostume} className={`costume-button ${showCostume ? 'active' : ''}`}>
+                    <img src="/images/costume_icon.png" alt="Toggle Costume" />
+                </button>
+                <button onClick={toggleShadow} className={`shadow-button ${showShadow ? 'active' : ''}`}>
+                    <img src="/images/shadow_icon.png" alt="Toggle Shadow" />
+                </button>
+            </div>
             <input 
                 type="text" 
                 placeholder="Search..."
                 value={searchTerm} 
                 onChange={(e) => onSearchChange(e.target.value)}
             />
-            <button onClick={toggleShiny} className={`shiny-button ${isShiny ? 'active' : ''}`}>
-                <img src="/images/shiny_icon.png" alt="Toggle Shiny" />
-            </button>
-            <button onClick={toggleCostume} className={`costume-button ${showCostume ? 'active' : ''}`}>
-                <img src="/images/costume_icon.png" alt="Toggle Costume" />
-            </button>
-            <button onClick={toggleShadow} className={`shadow-button ${showShadow ? 'active' : ''}`}>
-                <img src="/images/shadow_icon.png" alt="Toggle Shadow" />
-            </button>
-            {/* Evolutionary Line Checkbox */}
             <label className="evo-line-checkbox">
                 <input
                     id="evolutionaryLineCheckbox"
