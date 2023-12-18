8e290e31f4a8fd824de0e00cd0e7fafa4e705d10
Adam Wentworth <adamjohnwentworth@gmail.com>
2023-12-18 15:45:35 -0800

    added logo to navbar

--- frontend/src/components/mainButtons.jsx
+++ frontend/src/components/mainButtons.jsx
@@ -5,10 +5,10 @@ import './mainButtons.css';
 function MainButtons() {
     return (
         <div className="mainButtonsContainer">
+            <button className="mainButton pvp-btn">PVP</button>
             <Link to="/pokemon">
                 <button className="mainButton collect-btn">Collect</button>
             </Link>
-            <button className="mainButton pvp-btn">PVP</button>
             <button className="mainButton raid-btn">Raid</button>
         </div>
     );
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import './navbar.css';
 
 function Navbar() {
+    const logoUrl = process.env.PUBLIC_URL + '/images/logo/logo.jpg'; // Construct the URL
+
     return (
         <div className="navbar">
+            <div className="logo-container">
+                <img src={logoUrl} alt="Logo" /> {/* Use the URL instead of importing */}
+            </div>
             <div className="title-container">
                 <h1>Welcome to Pokémon Go Nexus</h1>
             </div>
