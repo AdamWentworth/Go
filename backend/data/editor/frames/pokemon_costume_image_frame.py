9aa36ab06f512287fdddef998438fe8b4de16a8f
Adam Wentworth <adamjohnwentworth@gmail.com>
2023-12-27 15:59:57 -0800

    got a semi functional version of the costume frame in the database editor

--- backend/data/editor/frames/pokemon_costume_image_frame.py
+++ backend/data/editor/frames/pokemon_costume_image_frame.py
@@ -1,7 +1,9 @@
+# pokemon_costume_image_frame.py
 import tkinter as tk
-from tkinter import filedialog, messagebox, ttk
+from tkinter import filedialog, messagebox, ttk, simpledialog
 from PIL import Image, ImageTk
 import requests
+import os
 from io import BytesIO
 
 class PokemonCostumeImageFrame(tk.Frame):
@@ -14,7 +16,14 @@ class PokemonCostumeImageFrame(tk.Frame):
         self.costumes = self.db_manager.fetch_pokemon_costumes(pokemon_id)
         self.images = {}  # To store the loaded images
         self.costume_entries = {}  # To store the entry widgets for costume attributes
-        self.initialize_ui()
+
+        # Set up the base directory for images
+        script_directory = os.path.dirname(os.path.realpath(__file__))
+        go_directory = os.path.normpath(os.path.join(script_directory, '../../../../'))
+        self.relative_path_to_images = os.path.join(go_directory, 'frontend', 'public')
+
+        self.initialize_ui()  # This now can be called safely after the above attributes are set
+        self.load_costume_images() 
 
     def initialize_ui(self):
         # UI setup for costumes
@@ -32,58 +41,105 @@ class PokemonCostumeImageFrame(tk.Frame):
                 entry.insert(0, str(costume[i+2]) if costume[i+2] is not None else "")
                 self.costume_entries[(costume[0], label)] = entry  # Store the entry with its costume_id and label
 
-            # Image display placeholder
+            # Image display placeholders
             image_label = tk.Label(frame)
             image_label.grid(row=0, column=2, rowspan=len(labels), padx=10, pady=10)
-            self.costume_frames.append((frame, image_label, costume[0]))  # Store frame, label, and costume_id for later
 
-            # Add update button for each costume
-            update_button = tk.Button(frame, text="Update Costume", command=lambda c_id=costume[0]: self.update_costume(c_id))
-            update_button.grid(row=len(labels), column=0, columnspan=3)
+            shiny_image_label = tk.Label(frame)
+            shiny_image_label.grid(row=0, column=3, rowspan=len(labels), padx=10, pady=10)
+            
+            # Store frame, labels, and costume_id for later
+            self.costume_frames.append((frame, image_label, shiny_image_label, costume[0]))
+            
+            # Add buttons for updating images from URL and file system
+            update_image_button = tk.Button(frame, text="Update Image from URL",
+                                            command=lambda c_id=costume[0]: self.update_costume_image_from_url(c_id))
+            update_image_button.grid(row=len(labels)+1, column=0, columnspan=2)
+
+            upload_image_button = tk.Button(frame, text="Upload Image from File",
+                                            command=lambda c_id=costume[0]: self.upload_costume_image(c_id))
+            upload_image_button.grid(row=len(labels)+1, column=2, columnspan=2)
 
         # Load and display images after setting up UI
         self.load_costume_images()
-
+    
     def load_costume_images(self):
-        
         # Load and display images for each costume
-        for index, (frame, image_label, costume_id) in enumerate(self.costume_frames):
+        for index, (frame, image_label, shiny_image_label, costume_id) in enumerate(self.costume_frames):
             costume = self.costumes[index]
-            image_url = costume[6]  # Assuming index 6 is image_url_costume
+            regular_image_path = costume[6]  # Regular image path (already relative)
+            shiny_image_path = costume[7]  # Shiny image path (already relative)
 
-            image = self.download_image(image_url) if image_url else None
+            # Open regular and shiny costume images if they exist, or display placeholder
+            image = self.open_local_image(regular_image_path) if regular_image_path else self.get_placeholder_image()
+            shiny_image = self.open_local_image(shiny_image_path) if shiny_image_path else self.get_placeholder_image()
+
             if image:
-                self.images[costume_id] = image  # Store the image with its costume_id
+                self.images[regular_image_path] = image  # Use the path as the key for regular images
+
+            if shiny_image:
+                self.images[shiny_image_path] = shiny_image  # Use the path as the key for shiny images
 
         self.display_costume_images()
+
+    def open_local_image(self, image_url):
+        # Trim leading slash from image_url if present to ensure it's treated as relative
+        image_url = image_url.lstrip("\\/")
+
+        # Combine the base path with the specific image URL
+        full_image_path = os.path.join(self.relative_path_to_images, image_url)
+
+        # Normalize the path to ensure consistent slashes
+        full_image_path = os.path.normpath(full_image_path)
+
+        # Try to open the image from the local filesystem
+        try:
+            image = Image.open(full_image_path)
+            return ImageTk.PhotoImage(image)
+        except FileNotFoundError:
+            messagebox.showerror("Image Error", f"Image file not found: {full_image_path}")
+            return self.get_placeholder_image()
+        except Exception as e:
+            messagebox.showerror("Image Error", f"Failed to open image: {e}")
+            return self.get_placeholder_image()
+
+    def get_placeholder_image(self):
+        # This method returns a placeholder image to be used when the image is missing
+        try:
+            placeholder = Image.open("path/to/placeholder.png")  # Update this path to your placeholder image
+            return ImageTk.PhotoImage(placeholder)
+        except IOError:
+            # If placeholder is also missing, you can create a blank image or show an error
+            return None
 
     def display_costume_images(self):
         # Update the UI with loaded images
-        for frame, image_label, costume_id in self.costume_frames:
-            image = self.images.get(costume_id)
-            if image:
-                image_label.configure(image=image)
-                image_label.image = image  # Keep a reference!
+        for frame, image_label, shiny_image_label, costume_id in self.costume_frames:
+            # Using the costume_id to fetch the paths directly from the images dictionary
+            costume = next((costume for costume in self.costumes if costume[0] == costume_id), None)
+            if costume is not None:
+                regular_image_path = costume[6]  # Regular image path
+                shiny_image_path = costume[7]  # Shiny image path
 
-    def update_costume_image(self, costume_id, new_image):
+                image = self.images.get(regular_image_path)
+                shiny_image = self.images.get(shiny_image_path)
 
-        # Update the specified costume image
-        # This will be a placeholder, you'll need to add the actual database update logic
+                if image:
+                    image_label.configure(image=image)
+                    image_label.image = image  # Keep a reference!
 
-        # Reload and redisplay the image
-        self.load_costume_images()
-        self.display_costume_images()
+                if shiny_image:
+                    shiny_image_label.configure(image=shiny_image)
+                    shiny_image_label.image = shiny_image  # Keep a reference!
 
-    def download_image(self, url):
-        # Download the image from the URL
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            image = Image.open(BytesIO(response.content))
-            return ImageTk.PhotoImage(image)
-        except requests.RequestException as e:
-            messagebox.showerror("Download Error", f"Failed to download image: {e}")
-            return None
+    def upload_costume_image(self, costume_id):
+        # Use the existing upload_image method to get the image from the file system
+        new_image = self.upload_image()
+        if new_image:
+            # Here you would update the database with the path of the new image
+            # self.db_manager.update_costume_image_file(costume_id, file_path)
+            self.images[costume_id] = new_image
+            self.display_costume_images()
 
     def upload_image(self):
         # Upload a new image from the user's device
