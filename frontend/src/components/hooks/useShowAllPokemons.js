7d06fa435d36a7edeb26420ff4fe039e46d11941
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-03-29 18:59:54 -0700

    added functionality for displaying variant string formatted names in the cards when show all enabled

--- frontend/src/components/hooks/useShowAllPokemons.js
+++ frontend/src/components/hooks/useShowAllPokemons.js
@@ -1,7 +1,8 @@
 // useShowAllPokemons.js
 
 import { useState, useEffect } from 'react';
-import { shouldAddPokemon, getEvolutionaryFamily } from '../../utils/searchFunctions';
+import { getEvolutionaryFamily } from '../../utils/searchFunctions';
+import { formatCostumeName } from '../../utils/formattingHelpers';
 
 const useShowAllPokemons = (allPokemons, filters, showEvolutionaryLine) => {
   const [variants, setVariants] = useState([]);
@@ -15,16 +16,16 @@ const useShowAllPokemons = (allPokemons, filters, showEvolutionaryLine) => {
 
       // Shiny variant
       if (pokemon.shiny_available) {
-        variants.push({ ...pokemon, currentImage: pokemon.image_url_shiny, variantType: 'shiny' });
+        variants.push({ ...pokemon, currentImage: pokemon.image_url_shiny, variantType: 'shiny', name: `Shiny ${pokemon.name}` });
       }
 
       // Shadow variant
       if (pokemon.date_shadow_available) {
-        variants.push({ ...pokemon, currentImage: pokemon.image_url_shadow, variantType: 'shadow' });
+        variants.push({ ...pokemon, currentImage: pokemon.image_url_shadow, variantType: 'shadow', name: `Shadow ${pokemon.name}` });
 
         // Shiny shadow variant
         if (pokemon.date_shiny_shadow_available) {
-          variants.push({ ...pokemon, currentImage: pokemon.image_url_shiny_shadow, variantType: 'shiny_shadow' });
+          variants.push({ ...pokemon, currentImage: pokemon.image_url_shiny_shadow, variantType: 'shiny_shadow', name: `Shiny Shadow ${pokemon.name}` });
         }
       }
 
@@ -32,11 +33,24 @@ const useShowAllPokemons = (allPokemons, filters, showEvolutionaryLine) => {
       if (pokemon.costumes) {
         pokemon.costumes.forEach(costume => {
           // Costume variant
-          variants.push({ ...pokemon, currentImage: costume.image_url, variantType: `costume_${costume.costume_id}` });
+          const formattedCostumeName = formatCostumeName(costume.name);
+          const costumeVariantName = `${formattedCostumeName} ${pokemon.name}`;
+          variants.push({
+            ...pokemon,
+            currentImage: costume.image_url,
+            variantType: `costume_${costume.costume_id}`,
+            name: costumeVariantName
+          });
 
           // Shiny costume variant
           if (costume.shiny_available) {
-            variants.push({ ...pokemon, currentImage: costume.image_url_shiny, variantType: `costume_${costume.costume_id}_shiny` });
+            const shinyCostumeVariantName = `Shiny ${formattedCostumeName} ${pokemon.name}`;
+            variants.push({
+              ...pokemon,
+              currentImage: costume.image_url_shiny,
+              variantType: `costume_${costume.costume_id}_shiny`,
+              name: shinyCostumeVariantName
+            });
           }
         });
       }
@@ -67,7 +81,7 @@ const useShowAllPokemons = (allPokemons, filters, showEvolutionaryLine) => {
     setVariants(allVariants);
   }, [allPokemons, filters.searchTerm, showEvolutionaryLine]);
 
-  // console.log(`Final variants for '${filters.searchTerm}':`, variants);
+  console.log(`Final variants for '${filters.searchTerm}':`, variants);
   return variants;
 };
 
