6e14bb8ef6f4b076f2b56fb46e099dc31d8627e1
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-04-22 12:57:25 -0700

    refactored searchFunctions, moving 2 functions out an into their own module called filterHelpers.js

---
+++ frontend/src/utils/filterHelpers.js
@@ -0,0 +1,60 @@
+//filterHelpers.js
+
+import { matchesSearchTerm } from './searchFunctions';
+
+export function getEvolutionaryFamily(searchTerm, allPokemons) {
+    const basePokemons = allPokemons.filter(p => p.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+    let family = new Set();
+
+    const findAllEvolutions = (pokemonId) => {
+        if (family.has(pokemonId)) {
+            return; // Skip if already processed
+        }
+
+        const pokemon = allPokemons.find(p => p.pokemon_id === pokemonId);
+        if (!pokemon) return;
+
+        // Add current pokemon to the family
+        family.add(pokemon.pokemon_id);
+
+        // Find evolutions
+        if (Array.isArray(pokemon.evolves_to)) {
+            pokemon.evolves_to.forEach(evolutionId => {
+                findAllEvolutions(evolutionId);
+            });
+        }
+
+        // Find pre-evolutions
+        if (Array.isArray(pokemon.evolves_from)) {
+            pokemon.evolves_from.forEach(preEvolutionId => {
+                findAllEvolutions(preEvolutionId);
+            });
+        }
+    };
+
+    basePokemons.forEach(basePokemon => findAllEvolutions(basePokemon.pokemon_id));
+
+    return Array.from(family);
+}
+
+
+export function shouldAddPokemon(pokemon, costume, selectedGeneration, isShiny, pokemonTypes, searchTerm, generations, showShadow) {
+    const matchesGeneration = selectedGeneration ? pokemon.generation === selectedGeneration : true;
+    const matchesShiny = isShiny && showShadow ? pokemon.shiny_available === 1 && pokemon.shadow_shiny_available === 1 : (isShiny ? pokemon.shiny_available === 1 : true);
+
+    const matchesSearch = matchesSearchTerm(pokemon, searchTerm, pokemonTypes, generations);
+
+    const basicMatches = matchesGeneration && matchesShiny && matchesSearch;
+
+    if (costume) {
+        return (
+            !isShiny && !showShadow ||
+            (isShiny && !showShadow && costume.shiny_available === 1) ||
+            (!isShiny && showShadow && costume.shadow_available === 1) ||
+            (isShiny && showShadow && costume.shiny_available === 1 && costume.shiny_shadow_available === 1)
+        ) && basicMatches;
+    } else {
+        return basicMatches;
+    }
+}
--- frontend/src/utils/searchFunctions.js
+++ frontend/src/utils/searchFunctions.js
@@ -34,28 +34,7 @@ export function handleSearchTermChange(allPokemons, term, generations, pokemonTy
     setFilteredPokemonList(results);
 }
 
-
-export function shouldAddPokemon(pokemon, costume, selectedGeneration, isShiny, pokemonTypes, searchTerm, generations, showShadow) {
-    const matchesGeneration = selectedGeneration ? pokemon.generation === selectedGeneration : true;
-    const matchesShiny = isShiny && showShadow ? pokemon.shiny_available === 1 && pokemon.shadow_shiny_available === 1 : (isShiny ? pokemon.shiny_available === 1 : true);
-
-    const matchesSearch = matchesSearchTerm(pokemon, searchTerm, pokemonTypes, generations);
-
-    const basicMatches = matchesGeneration && matchesShiny && matchesSearch;
-
-    if (costume) {
-        return (
-            !isShiny && !showShadow ||
-            (isShiny && !showShadow && costume.shiny_available === 1) ||
-            (!isShiny && showShadow && costume.shadow_available === 1) ||
-            (isShiny && showShadow && costume.shiny_available === 1 && costume.shiny_shadow_available === 1)
-        ) && basicMatches;
-    } else {
-        return basicMatches;
-    }
-}
-
-function matchesSearchTerm(pokemon, searchTerm, pokemonTypes, generations) {
+export function matchesSearchTerm(pokemon, searchTerm, pokemonTypes, generations) {
 
     const unionTerms = searchTerm.split(',').map(term => term.trim().toLowerCase());
 
@@ -74,7 +53,7 @@ function matchesSearchTerm(pokemon, searchTerm, pokemonTypes, generations) {
 }
 
 
-function checkTermMatches(pokemon, term, pokemonTypes, generationMap) {
+export function checkTermMatches(pokemon, term, pokemonTypes, generationMap) {
     const isNegation = term.startsWith('!');
 
     if (isNegation) {
@@ -103,42 +82,6 @@ function checkTermMatches(pokemon, term, pokemonTypes, generationMap) {
 
     // If the term had a '!', negate the result
     return isNegation ? !result : result;
-}
-
-export function getEvolutionaryFamily(searchTerm, allPokemons) {
-    const basePokemons = allPokemons.filter(p => p.name.toLowerCase().includes(searchTerm.toLowerCase()));
-
-    let family = new Set();
-
-    const findAllEvolutions = (pokemonId) => {
-        if (family.has(pokemonId)) {
-            return; // Skip if already processed
-        }
-
-        const pokemon = allPokemons.find(p => p.pokemon_id === pokemonId);
-        if (!pokemon) return;
-
-        // Add current pokemon to the family
-        family.add(pokemon.pokemon_id);
-
-        // Find evolutions
-        if (Array.isArray(pokemon.evolves_to)) {
-            pokemon.evolves_to.forEach(evolutionId => {
-                findAllEvolutions(evolutionId);
-            });
-        }
-
-        // Find pre-evolutions
-        if (Array.isArray(pokemon.evolves_from)) {
-            pokemon.evolves_from.forEach(preEvolutionId => {
-                findAllEvolutions(preEvolutionId);
-            });
-        }
-    };
-
-    basePokemons.forEach(basePokemon => findAllEvolutions(basePokemon.pokemon_id));
-
-    return Array.from(family);
 }
 
 
