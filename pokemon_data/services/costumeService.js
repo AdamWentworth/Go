f6e46d42713cb1c7cb8cf35d60d427962e94efcd
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-05-30 00:10:35 -0700

    added shadow_costume object to each costume. its usually null but for the 9 instances there are shadow_costumes, it will store all their details from the database there

--- pokemon_data/services/costumeService.js
+++ pokemon_data/services/costumeService.js
@@ -1,5 +1,9 @@
 function getCostumesForPokemon(db, callback) {
-    const costumeQuery = "SELECT * FROM costume_pokemon";
+    const costumeQuery = `
+        SELECT cp.*, scp.date_available as shadow_date_available, scp.date_shiny_available as shadow_date_shiny_available, 
+               scp.image_url_shadow_costume, scp.image_url_shiny_shadow_costume
+        FROM costume_pokemon cp
+        LEFT JOIN shadow_costume_pokemon scp ON cp.costume_id = scp.costume_id`;
 
     db.all(costumeQuery, [], (err, costumes) => {
         if (err) {
@@ -17,11 +21,17 @@ function formatCostumes(pokemonsWithImages, costumes) {
         const formattedCostumes = relatedCostumes.map(c => ({
             costume_id: c.costume_id,
             name: c.costume_name,
-            image_url: c.image_url_costume, // Directly using the URL from the database
-            image_url_shiny: c.image_url_shiny_costume, // Directly using the URL from the database
+            image_url: c.image_url_costume,
+            image_url_shiny: c.image_url_shiny_costume,
             shiny_available: c.shiny_available,
             date_available: c.date_available,
-            date_shiny_available: c.date_shiny_available
+            date_shiny_available: c.date_shiny_available,
+            shadow_costume: c.image_url_shadow_costume ? {
+                date_available: c.shadow_date_available,
+                date_shiny_available: c.shadow_date_shiny_available,
+                image_url_shadow_costume: c.image_url_shadow_costume,
+                image_url_shiny_shadow_costume: c.image_url_shiny_shadow_costume
+            } : null  // Only add shadow costume object if the data exists
         }));
 
         return {
