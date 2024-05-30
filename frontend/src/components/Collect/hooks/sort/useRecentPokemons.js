77602af4e9896d3e8fb32b8d097435245e1854e3
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-05-30 01:17:39 -0700

    updated recent sorting to include considerations for the shadow costume pokemon

--- frontend/src/components/Collect/hooks/sort/useRecentPokemons.js
+++ frontend/src/components/Collect/hooks/sort/useRecentPokemons.js
@@ -7,7 +7,15 @@ const useRecentPokemons = (displayedPokemons, sortMode, { isShiny, showShadow, s
         // Convert 'ascending' or 'descending' to appropriate sort order
         const sortOrder = sortMode === 'ascending' ? 1 : -1;
 
-        return [...displayedPokemons].sort((a, b) => {
+        // Filter out non-shadow costumes if both showShadow and showCostume are true
+        const filteredPokemons = showShadow && showCostume ? 
+            displayedPokemons.filter(pokemon => 
+                pokemon.variantType.startsWith('shadow_costume') && 
+                pokemon.costumes.some(costume => costume.shadow_costume)
+            ) : 
+            displayedPokemons;
+
+        return filteredPokemons.sort((a, b) => {
             let dateA, dateB;
 
             const getSortDate = (pokemon) => {
@@ -18,8 +26,15 @@ const useRecentPokemons = (displayedPokemons, sortMode, { isShiny, showShadow, s
                         const costumeData = pokemon.costumes.find(costume => costume.costume_id === costumeId);
                         if (costumeData) {
                             return new Date(isShinyVariant ? costumeData.date_shiny_available : costumeData.date_available);
+                        }}
+                    if (pokemon.variantType.startsWith('shadow_costume')) {
+                        const costumeId = parseInt(pokemon.variantType.match(/\d+/)[0], 10);
+                        const costumeData = pokemon.costumes.find(costume => costume.costume_id === costumeId);
+                        if (costumeData) {
+                            return new Date(costumeData.shadow_costume.date_available);
                         }
-                    } else {
+                    }
+                    else {
                         switch (pokemon.variantType) {
                             case 'default': return new Date(pokemon.date_available);
                             case 'shiny': return new Date(pokemon.date_shiny_available);
@@ -36,6 +51,13 @@ const useRecentPokemons = (displayedPokemons, sortMode, { isShiny, showShadow, s
                             const date = isShiny ? costumeData.date_shiny_available : costumeData.date_available;
                             return new Date(date);
                         }
+                        if (pokemon.variantType.startsWith('shadow_costume')) {
+                            const costumeId = parseInt(pokemon.variantType.match(/\d+/)[0], 10);
+                            const costumeData = pokemon.costumes.find(costume => costume.costume_id === costumeId);
+                            if (costumeData) {
+                                return new Date(costumeData.shadow_costume.date_available);
+                            }
+                        }
                     }
                 }
                 if (isShiny && showShadow && pokemon.date_shiny_shadow_available) {
@@ -45,7 +67,25 @@ const useRecentPokemons = (displayedPokemons, sortMode, { isShiny, showShadow, s
                     return new Date(pokemon.date_shiny_available);
                 }
                 if (showShadow && pokemon.date_shadow_available) {
+                    if (pokemon.variantType.startsWith('shadow_costume')) {
+                        const costumeId = parseInt(pokemon.variantType.match(/\d+/)[0], 10);
+                        const costumeData = pokemon.costumes.find(costume => costume.costume_id === costumeId);
+                        if (costumeData) {
+                            return new Date(costumeData.shadow_costume.date_available);
+                        }
+                    }
+                    else {
                     return new Date(pokemon.date_shadow_available);
+                    }
+                }
+                if (showShadow && showCostume) {
+                    if (pokemon.variantType.startsWith('shadow_costume')) {
+                        const costumeId = parseInt(pokemon.variantType.match(/\d+/)[0], 10);
+                        const costumeData = pokemon.costumes.find(costume => costume.costume_id === costumeId);
+                        if (costumeData) {
+                            return new Date(costumeData.shadow_costume.date_available);
+                        }
+                    }
                 }
                 return new Date(pokemon.date_available);
             };
