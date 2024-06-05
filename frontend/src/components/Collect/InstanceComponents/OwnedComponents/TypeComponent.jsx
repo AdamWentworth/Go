cde44f4bfd29c17fa57478cfd7126774605ccc9e
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-06-05 08:41:23 -0700

    added a good type Component

--- frontend/src/components/Collect/InstanceComponents/OwnedComponents/TypeComponent.jsx
+++ frontend/src/components/Collect/InstanceComponents/OwnedComponents/TypeComponent.jsx
@@ -1,11 +1,22 @@
 // TypeComponent.jsx
 import React from 'react';
+import './TypeComponent.css';
 
 const TypeComponent = ({ pokemon }) => {
+  const type1 = pokemon.type1_name;
+  const type2 = pokemon.type2_name;
+  const type1Icon = pokemon.type_1_icon;
+  const type2Icon = pokemon.type_2_icon;
+
   return (
-    <div className="type-component">
-      <label className="type-label">Type: </label>
-      {/* <TypeIcons pokemon={pokemon} /> */}
+    <div className="type-container">
+      <div className="type-icons">
+        {type1 && <img src={type1Icon} alt={type1} className="type-icon" />}
+        {type2 && <img src={type2Icon} alt={type2} className="type-icon" />}
+      </div>
+      <div className="type-label">
+        {type1}{type2 ? ` / ${type2}` : ''}
+      </div>
     </div>
   );
 };
