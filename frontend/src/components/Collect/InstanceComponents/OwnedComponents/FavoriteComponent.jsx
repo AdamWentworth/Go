83eab77cef7932b3c1a77aee7289dd3d40f6291c
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-06-06 18:53:00 -0700

    added FavoriteComponent

--- frontend/src/components/Collect/InstanceComponents/OwnedComponents/FavoriteComponent.jsx
+++ frontend/src/components/Collect/InstanceComponents/OwnedComponents/FavoriteComponent.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+// FavoriteComponent.jsx
+import React, { useState, useEffect } from 'react';
+import './FavoriteComponent.css'
 
 const FavoriteComponent = ({ pokemon }) => {
-    return (
-        <div className="favorite-component">
-            {/* Placeholder content */}
-        </div>
-    );
+  // Set initial favorite state based on the pokemon's favorite status
+  const [isFavorite, setIsFavorite] = useState(pokemon.ownershipStatus.favorite);
+
+  // Determine the correct image based on the favorite status
+  const favoriteImage = isFavorite
+    ? process.env.PUBLIC_URL + '/images/fav_pressed.png'
+    : process.env.PUBLIC_URL + '/images/fav_unpressed.png';
+
+  // Toggle favorite status and image
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
+  return (
+    <div className="favorite-component" onClick={toggleFavorite}>
+      <img src={favoriteImage} alt={isFavorite ? 'Favorite' : 'Not Favorite'} />
+    </div>
+  );
 }
 
 export default FavoriteComponent;
