890ff13f4c4aeb74a070eb538ef5b9e0547e1460
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-08-08 18:42:11 -0700

    moved receiver out of user_pokemon_management and tested. still works for user_pokemon_management, but now is semantically more prepared for adding location data receiving and message producing for cross service kafka message consumption

---
+++ receiver/manage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def execute_command(command):
+    # Set the DJANGO_SETTINGS_MODULE environment variable
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'receiver.settings'
+    
+    # Ensure the current directory is in the Python path
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, current_path)
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    execute_from_command_line(command)
+
+def main():
+    if len(sys.argv) < 2:
+        # If no command is provided, run the server on port 3003 by default
+        command = ['runserver', '3003']
+    else:
+        command = sys.argv[1:]
+    
+    execute_command([sys.argv[0]] + command)
+
+if __name__ == '__main__':
+    main()
