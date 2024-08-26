6b547a4bb12f762e5a7996c1b349b5daf91c42d0
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-08-26 14:40:29 -0700

    made it so that search bar doesnt interact with main thread until after a .25s delay when a user stops typing

--- frontend/src/components/Collect/UIComponents/SearchUI.jsx
+++ frontend/src/components/Collect/UIComponents/SearchUI.jsx
@@ -1,21 +1,33 @@
 /*searchUI.jsx*/
 
-import React from 'react';
+import React, { useCallback } from 'react';
+import { debounce } from 'lodash';
 import './SearchUI.css'; // Your CSS file should include the styles for the evolutionary line checkbox
 
 function SearchUI({
     searchTerm,
     onSearchChange,
-    showEvolutionaryLine, // state to track if the checkbox is checked
-    toggleEvolutionaryLine, // function to handle checkbox changes
+    showEvolutionaryLine,
+    toggleEvolutionaryLine,
 }) {
+    // Use useCallback to memoize the debounced function
+    const debouncedSearchChange = useCallback(
+        debounce((value) => onSearchChange(value), 250),
+        []
+    );
+
+    const handleInputChange = (e) => {
+        const value = e.target.value;
+        debouncedSearchChange(value);
+    };
+
     return (
         <div className="header-section search-section">
             <input 
                 type="text" 
                 placeholder="Search..."
-                value={searchTerm} 
-                onChange={(e) => onSearchChange(e.target.value)}
+                defaultValue={searchTerm} 
+                onChange={handleInputChange}
             />
             <label className="evo-line-checkbox">
                 <div className="checkbox-container">
