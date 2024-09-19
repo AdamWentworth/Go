749773a5e5887518417c0d267bb7713f828b5061
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-09-18 23:05:11 -0700

    moved backups of services out of repo but still on desktop machine

--- django-backup/reader/reader/urls.py
+++
@@ -1,8 +0,0 @@
-# reader/urls.py
-
-from django.urls import path
-from .views import UserPokemonList
-
-urlpatterns = [
-    path('api/ownershipData/<str:user_id>', UserPokemonList.as_view(), name='user-pokemon-list'),  # Removed trailing slash
-]
--- django-backup/receiver/receiver/urls.py
+++
@@ -1,8 +0,0 @@
-# receiver/urls.py
-
-from django.urls import path
-from .views import handle_batched_updates
-
-urlpatterns = [
-    path('api/batchedUpdates', handle_batched_updates, name='batched_updates'),
-]
