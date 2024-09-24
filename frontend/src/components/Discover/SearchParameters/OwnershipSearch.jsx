f2f5f5f7c24b0953a36c8235011033f36c408f6b
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-09-23 18:55:55 -0700

    added appropriate colors to the ownership status buttons

--- frontend/src/components/Discover/SearchParameters/OwnershipSearch.jsx
+++ frontend/src/components/Discover/SearchParameters/OwnershipSearch.jsx
@@ -1,40 +1,23 @@
 // OwnershipSearch.jsx
-
 import React from 'react';
 import './OwnershipSearch.css'; // Import the CSS file for OwnershipSearch styling
 
 const OwnershipSearch = ({ ownershipStatus, setOwnershipStatus }) => {
+  const options = ['owned', 'trade', 'wanted']; // Define the options
+
   return (
     <div className="ownership-status">
       <h3 className="ownership-header">Ownership Status</h3>
       <div className="ownership-options">
-        <label className="ownership-option">
-          <input
-            type="radio"
-            value="owned"
-            checked={ownershipStatus === 'owned'}
-            onChange={() => setOwnershipStatus('owned')}
-          />
-          Owned
-        </label>
-        <label className="ownership-option">
-          <input
-            type="radio"
-            value="trade"
-            checked={ownershipStatus === 'trade'}
-            onChange={() => setOwnershipStatus('trade')}
-          />
-          Trade
-        </label>
-        <label className="ownership-option">
-          <input
-            type="radio"
-            value="wanted"
-            checked={ownershipStatus === 'wanted'}
-            onChange={() => setOwnershipStatus('wanted')}
-          />
-          Wanted
-        </label>
+        {options.map((option) => (
+          <button
+            key={option}
+            className={`ownership-button ${ownershipStatus === option ? 'active ' + option : 'inactive ' + option}`}
+            onClick={() => setOwnershipStatus(option)}
+          >
+            {option.charAt(0).toUpperCase() + option.slice(1)} {/* Capitalize first letter */}
+          </button>
+        ))}
       </div>
     </div>
   );
