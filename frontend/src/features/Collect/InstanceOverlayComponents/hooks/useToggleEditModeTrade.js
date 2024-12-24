df93f443fe24b057f87447da293e83ce525232f5
Adam Wentworth <adamjohnwentworth@gmail.com>
2024-12-23 18:12:52 -0800

    tidied up the noisy logs

--- frontend/src/features/Collect/InstanceOverlayComponents/hooks/useToggleEditModeTrade.js
+++ frontend/src/features/Collect/InstanceOverlayComponents/hooks/useToggleEditModeTrade.js
@@ -23,23 +23,23 @@ const useToggleEditModeTrade = (
     const [editMode, setEditMode] = useState(false);
 
     const toggleEditMode = () => {
-        console.log('--- toggleEditMode TRIGGERED ---');
-        console.log('Current editMode value:', editMode);
-        console.log('pokemonKey:', pokemon.pokemonKey);
-        console.log('OwnershipStatus.not_wanted_list BEFORE all changes:', pokemon.ownershipStatus.not_wanted_list);
-        console.log('localNotWantedList BEFORE all changes:', localNotWantedList);    
+        // console.log('--- toggleEditMode TRIGGERED ---');
+        // console.log('Current editMode value:', editMode);
+        // console.log('pokemonKey:', pokemon.pokemonKey);
+        // console.log('OwnershipStatus.not_wanted_list BEFORE all changes:', pokemon.ownershipStatus.not_wanted_list);
+        // console.log('localNotWantedList BEFORE all changes:', localNotWantedList);    
 
         if (editMode) {
             const updatedNotWantedList = { ...localNotWantedList };
 
             // Make sure to log right here:
-            console.log('updatedNotWantedList AFTER copy from localNotWantedList:', updatedNotWantedList);
+            // console.log('updatedNotWantedList AFTER copy from localNotWantedList:', updatedNotWantedList);
 
             filteredOutPokemon.forEach(key => {
                 updatedNotWantedList[key] = true;
             });
 
-            console.log('updatedNotWantedList AFTER setting filteredOutPokemon to true:', updatedNotWantedList);
+            // console.log('updatedNotWantedList AFTER setting filteredOutPokemon to true:', updatedNotWantedList);
     
 
             const removedKeys = Object.keys(pokemon.ownershipStatus.not_wanted_list).filter(
@@ -49,16 +49,16 @@ const useToggleEditModeTrade = (
                 key => !pokemon.ownershipStatus.not_wanted_list[key]
               );
               
-              console.log('removedKeys:', removedKeys);
-              console.log('addedKeys:', addedKeys);              
+            //   console.log('removedKeys:', removedKeys);
+            //   console.log('addedKeys:', addedKeys);              
 
             const updatesToApply = {};
 
             removedKeys.forEach(key => {
-                console.log(
-                  'Removing Key from notWantedList -> updateNotTradeList with false:',
-                  { key, pokemonKey: pokemon.pokemonKey }
-                );
+                // console.log(
+                //   'Removing Key from notWantedList -> updateNotTradeList with false:',
+                //   { key, pokemonKey: pokemon.pokemonKey }
+                // );
                 const updatedNotTradeList = updateNotTradeList(
                   ownershipData,
                   pokemon.pokemonKey,
@@ -66,7 +66,7 @@ const useToggleEditModeTrade = (
                   false,
                   isMirror
                 );
-                console.log('updatedNotTradeList (REMOVAL) returned:', updatedNotTradeList);
+                // console.log('updatedNotTradeList (REMOVAL) returned:', updatedNotTradeList);
 
                 if (updatedNotTradeList) {
                     updatesToApply[key] = {
@@ -76,10 +76,10 @@ const useToggleEditModeTrade = (
             });
 
             addedKeys.forEach(key => {
-                console.log(
-                  'Adding Key to notWantedList -> updateNotTradeList with true:',
-                  { key, pokemonKey: pokemon.pokemonKey }
-                );
+                // console.log(
+                //   'Adding Key to notWantedList -> updateNotTradeList with true:',
+                //   { key, pokemonKey: pokemon.pokemonKey }
+                // );
                 const updatedNotTradeList = updateNotTradeList(
                   ownershipData,
                   pokemon.pokemonKey,
@@ -87,7 +87,7 @@ const useToggleEditModeTrade = (
                   true,
                   isMirror
                 );
-                console.log('updatedNotTradeList (ADDITION) returned:', updatedNotTradeList);
+                // console.log('updatedNotTradeList (ADDITION) returned:', updatedNotTradeList);
 
                 if (updatedNotTradeList) {
                     updatesToApply[key] = {
@@ -103,17 +103,17 @@ const useToggleEditModeTrade = (
             };
 
             if (!isMirror && mirrorKey) {
-                console.log('Handling Mirror Key Management');
+                // console.log('Handling Mirror Key Management');
                 delete ownershipData[mirrorKey];
                 delete lists.wanted[mirrorKey];
                 updateDisplayedList(null, listsState, setListsState);
                 setMirrorKey(null);
             }
 
-            console.log('Final updatesToApply:', updatesToApply);
+            // console.log('Final updatesToApply:', updatesToApply);
 
             updateDetails([...removedKeys, ...addedKeys, pokemon.pokemonKey], updatesToApply);
-            console.log('Called updateDetails with these keys:', [...removedKeys, ...addedKeys, pokemon.pokemonKey]);
+            // console.log('Called updateDetails with these keys:', [...removedKeys, ...addedKeys, pokemon.pokemonKey]);
             setLocalNotWantedList(updatedNotWantedList);
         } else {
             if (!isMirror && pokemon.ownershipStatus.mirror) {
@@ -124,13 +124,13 @@ const useToggleEditModeTrade = (
             }
         }
 
-        console.log('Local not_wanted_list after toggleEditMode:', localNotWantedList);
+        // console.log('Local not_wanted_list after toggleEditMode:', localNotWantedList);
         setEditMode(!editMode);
         pokemon.ownershipStatus.not_wanted_list = localNotWantedList;
-        console.log(
-            'pokemon.ownershipStatus.not_wanted_list AFTER assignment:',
-            pokemon.ownershipStatus.not_wanted_list
-          );          
+        // console.log(
+        //     'pokemon.ownershipStatus.not_wanted_list AFTER assignment:',
+        //     pokemon.ownershipStatus.not_wanted_list
+        //   );          
     };
 
     return { editMode, toggleEditMode };
--- frontend/src/features/Collect/InstanceOverlayComponents/utils/ReciprocalUpdate.js
+++ frontend/src/features/Collect/InstanceOverlayComponents/utils/ReciprocalUpdate.js
@@ -9,22 +9,13 @@ export const updateNotTradeList = (ownershipData, currentPokemonKey, otherPokemo
     // console.log(`updateNotTradeList called for ${otherPokemonKey} with add=${add}`);
 
     // Fetch the current not_trade_list from the ownership data
-    const notTradeList = ownershipData[otherPokemonKey].not_trade_list || {};    
+    const notTradeList = ownershipData[otherPokemonKey].not_trade_list || {};
 
-    if (isMirror) {
-        // When mirroring, ensure the original instance is excluded
-        Object.keys(ownershipData).forEach(key => {
-            if (key == currentPokemonKey) {
-                notTradeList[key] = add;
-            }
-        });
+    // Update the not_trade_list based on the 'add' parameter
+    if (add) {
+        notTradeList[currentPokemonKey] = true;
     } else {
-        // Update the not_trade_list based on the 'add' parameter
-        if (add) {
-            notTradeList[currentPokemonKey] = true;
-        } else {
-            delete notTradeList[currentPokemonKey];  // Remove the entry if `add` is false
-        }
+        delete notTradeList[currentPokemonKey];  // Remove the entry if `add` is false
     }
 
     // Commit the updated not_trade_list back to the ownership data
